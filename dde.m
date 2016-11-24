16aebc81b5df704b27e1ee6096a89e86e1cc90fd
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2016-11-24 17:01:33 +0100

    a critique?

--- dde.m
+++ dde.m
@@ -4,18 +4,18 @@ function sol=dde(gamma,delta,f0,n,A,theta,tspan)
         R=y(2);
         P=y(1);
         RA=Z(2,2);
-        dydt=[-gamma.*P+Hill(R).*R-exp(-gamma.*A).*Hill(RA).*RA ;
-            -delta.*R-Hill(R).*R+2.*exp(-gamma.*A).*Hill(RA).*RA];
+        dydt=[-gamma*P+Hill(R)*R-exp(-gamma*A)*Hill(RA)*RA ;
+            -delta*R-Hill(R)*R+2*exp(-gamma*A)*Hill(RA)*RA];
     end
 
     function f=Hill(r)
-       f=f0.*theta.^n./(r.^n+theta.^n);
+       f=f0*theta^n/(r^n+theta^n);
     end
 
     function s = ddhist(t)
     % Constant history function for DDEX1.
-        s = ones(2,1);
+        s = 10*ones(2,1);
     end
 
-    sol=dde23(@equation,[A,A],@ddhist,[0,10]);
+    sol=dde23(@equation,[10^-30,A],@ddhist,tspan);
 end
--- main3.m
+++ main3.m
@@ -3,13 +3,20 @@ gamma=0.2;
 delta=0.05;
 theta=10^11;
 n=5;
-A=10;
+%A=1;
 
-tspan=[0,100];
-sol=dde(gamma,delta,f0,n,A,theta,tspan);
+%Recherche du A critique
+Ac=a_c(f0,gamma,delta,theta,n)
+
+
+tspan=[0,1000];
+sol=dde(gamma,delta,f0,n,Ac,theta,tspan);
+ r_e=theta*(f0*(2*exp(-gamma*Ac)-1)/delta-1)^(1/n)
 figure(1);
 plot(sol.x,sol.y);
-title('A=1');
+title(strcat('A=',num2str(Ac)));
 xlabel('time t');
 ylabel('solution y');
 legend('P(t)','R(t)');
+
+
