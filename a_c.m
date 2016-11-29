d757aeb723a4dca0275ee1d8cd384790687fac05
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2016-11-29 20:28:43 +0100

    Ajout differents graphes, meilleur calcul du Ac

--- a_c.m
+++ a_c.m
@@ -1,10 +1,12 @@
 function ac=a_c(f0,gamma,delta,theta,n)
     function A_e=a_et(A)
         r_e=theta*(f0*(2*exp(-gamma*A)-1)/delta-1)^(1/n); % equilibre
-        B=f0*theta^n*(1+n.*r_e.^(n)./(r_e.^n+theta.^n))./(r_e.^n+theta.^n);
-        alpha=delta;
-        beta=2*1*exp(-gamma*A)+alpha;
-        A_e=1/sqrt(beta^2-alpha^2)*acos(alpha/beta)-A;
+        B=f0*theta^n*(1-n*r_e^(n)/(r_e^n+theta^n))/(r_e^n+theta^n);
+        alpha=delta+B;
+        beta=-2*B*exp(-gamma*A);
+        A_e=acos(-alpha/beta)/sqrt(beta^2-alpha^2)-A
     end
-ac=fzero(@a_et,0);
+ac=fzero(@a_et,1);
 end
+
+
