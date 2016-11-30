b2900bc5919050a564278b88ae65b8e2f238445a
VASSAL Hélène <helene.vassal@insa-lyon.fr>
2016-11-30 10:29:35 +0100

    observations

--- a_c.m
+++ a_c.m
@@ -4,7 +4,7 @@ function ac=a_c(f0,gamma,delta,theta,n)
         B=f0*theta^n*(1-n*r_e^(n)/(r_e^n+theta^n))/(r_e^n+theta^n);
         alpha=delta+B;
         beta=-2*B*exp(-gamma*A);
-        A_e=acos(-alpha/beta)/sqrt(beta^2-alpha^2)-A
+        A_e=acos(-alpha/beta)/sqrt(beta^2-alpha^2)-A;
     end
 ac=fzero(@a_et,1);
 end
