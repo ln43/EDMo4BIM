99561f688ce4bda4fb0f4261bfa7f954ba10c895
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2016-12-22 19:05:23 +0100

    Ajout modelisation stable partie 2

--- main3.m
+++ main3.m
@@ -3,14 +3,14 @@ gamma=0.2;
 delta=0.05;
 theta=10^11;
 n=5;
-tmax=10000;
+tmax=2000;
 
 %Recherche du A critique
 Ac=a_c(f0,gamma,delta,theta,n)
 
 tspan=[0,tmax];
 i=1;
-for A=linspace(Ac,Ac+0.005,9)
+for A=linspace(Ac-1,Ac+1,9)
     N=length(0.5:0.5:5);
     sol=dde(gamma,delta,f0,n,A,theta,tspan);
     r_e=theta*(f0*(2*exp(-gamma*A)-1)/delta-1)^(1/n);
@@ -25,20 +25,20 @@ for A=linspace(Ac,Ac+0.005,9)
     ylabel('solution y');
     legend('P(t)','R(t)');
     
-%     figure(2)
-%     subplot(3,3,i);
-%     plot(Y(1,:),Y(2,:));
-%     title(strcat('A=',num2str(A)));
-%     xlabel('P(t)');
-%     ylabel('R(t)');
-%     
-%     figure(3)
-%     subplot(3,3,i);
-%     plot3(t,Y(1,:),Y(2,:));
-%     title(strcat('A=',num2str(A)));
-%     xlabel('t');
-%     ylabel('P(t)');
-%     zlabel('R(t)');
+    figure(2)
+    subplot(3,3,i);
+    plot(Y(1,:),Y(2,:));
+    title(strcat('A=',num2str(A)));
+    xlabel('P(t)');
+    ylabel('R(t)');
+    
+    figure(3)
+    subplot(3,3,i);
+    plot3(t,Y(1,:),Y(2,:));
+    title(strcat('A=',num2str(A)));
+    xlabel('t');
+    ylabel('P(t)');
+    zlabel('R(t)');
 %     
 %     figure(4)
 %     subplot(3,3,i);
@@ -61,5 +61,5 @@ end
 
 % Graphiquement on trouve Ac autour de 1.789
 
-
+'FIN'
 
