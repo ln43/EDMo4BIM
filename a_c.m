cb2c34d3b7ac3ec203c250e7a3703b905c499319
VASSAL Hélène <helene.vassal@insa-lyon.fr>
2017-01-03 17:59:22 +0100

    Modif lundi 03/01

--- a_c.m
+++ a_c.m
@@ -1,20 +1,20 @@
 %Fonction permettant de calculer la valeur du A critique pour l'apparition
-%des oscillations en fonction des différents paramètres du système
+%des oscillations en fonction des differents parametres du systeme
 
-% alpha et beta sont les coefficients de l'equation caractéristique :
+% alpha et beta sont les coefficients de l'equation caracteristique :
 % lambda+alpha+beta*exp(-lambda A)
 
-% B est defini comme f'(R*)R*+f(R*) où R* est l'equilibre
+% B est defini comme f'(R*)R*+f(R*) ou R* est l'equilibre
 
-% Les oscillations non amorties sont observées pour A=A*
+% Les oscillations non amorties sont observees pour A=A*
 % A^*=arccos(-alpha+beta)/sqrt(beta^2-alpha^2)
 
 function ac=a_c(f0,gamma,delta,theta,n)
     function A_e=a_et(A)
         r_e=theta*(f0*(2*exp(-gamma*A)-1)/delta-1)^(1/n); % Equilibre R
         B=f0*theta^n*(1-n*r_e^(n)/(r_e^n+theta^n))/(r_e^n+theta^n);
-        alpha=-gamma+delta+B;
-        beta=-2*B;
+        alpha=delta+B;
+        beta=-2*B*exp(-gamma*A);
         A_e=acos(-alpha/beta)/sqrt(beta^2-alpha^2)-A;
     end
 ac=fzero(@a_et,-1);%On veut obtenir A*-A=0
--- dde.m
+++ dde.m
@@ -1,3 +1,12 @@
+%Resoud l'equation differentielle :
+%        P'(t)=-gamma P(t)+f(R(t))R(t)-e^(-gamma A) f(RA(t)) RA(t)
+%        R'(t)=-delta R(t)-f(R(t))R(t)+2e^(-gamma A) f(RA(t)) RA(t)
+%   avec f(R(t))=f0*theta^n/(R(t)^n+theta^n)
+%   et RA(t)=R(t-A)
+% sur l'intervalle de temps tspan avec pour conditions initials:
+%       P(t)=1, R(t)=1 quelque soit t<=A
+% Utilisation du solver dde23
+
 function sol=dde(gamma,delta,f0,n,A,theta,tspan)
 
     function dydt=equation(t,y,Z)
@@ -16,7 +25,7 @@ function sol=dde(gamma,delta,f0,n,A,theta,tspan)
     function s = ddhist(t)
     % Constant history function for DDEX1.
     
-        s =theta*ones(2,1);
+        s =ones(2,1);
     end
 
     sol=dde23(@equation,A,@ddhist,tspan);
