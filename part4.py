bfcacdb3a8155ca8060d52a66e457264a58859ba
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2017-01-11 20:36:19 +0100

    Ajout commentaire partie4

--- part4.py
+++ part4.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
+# Fonction de Hill
+# @param r
+# @return f(r) avec f la fonction de Hill
 def hill(r):
     return f0*(theta**n/(theta**n+r**n))
     
+ # Integrale de la fonction de Hill entre tinf et tsup
+# @param tinf borne inferieure
+# @param tsup borne superieure
+# @return I l'integrale de la fonction
 def int_hill(tinf,tsup,r):
     I=0
     t=tinf
@@ -12,7 +19,13 @@ def int_hill(tinf,tsup,r):
         I+=hill(r[ind])*h
         t+=h
     return I
-    
+ 
+ # Integrale qui fait partie de l'equation a retard
+ # @param amax borne sup
+ # @param r la valeur de R calculee aux temps precents
+ # @param ra la valeur de R_A calculee aux temps precedents 
+ # @param t le temps de l'iteration courante
+ # @return l'integrale calculee  
 def int_r(amax,r,ra,t):
     I=0
     a=0
@@ -22,7 +35,7 @@ def int_r(amax,r,ra,t):
     return I
 
 
-
+######### PARAMETRES
 global A,f0,gamma,delta,theta,n,h;
 A=1.5
 f0=1.77;
@@ -32,11 +45,13 @@ theta=10**11;
 n=5;
 amax=50
 h=0.05
-r=[10**11]*int(1+amax/h)
-len(r)
-ra=list(np.ones(A/h))+r
+r=[10**11]*int(1+amax/h) # Condition initiale pour R
+ra=list(np.ones(A/h))+r 
 tmax=25
 
+print "Lancement de la simulation"
+
+######## SIMULATIONS
 t=0
 while t<tmax:
     R=r[int((t+amax)/h)]+h*2*hill(ra[int((t+amax)/h)])*ra[int((t+amax)/h)]*np.exp(-gamma*A**2/2)-h*int_r(amax,r,ra,t)
@@ -46,11 +61,10 @@ while t<tmax:
         r.append(0)
     ra.append(r[len(r)-1])
     t+=h
-    print r[len(r)-1]
+    
 
-
+######## AFFICHAGE
 x=np.arange(0,tmax+h,h)
-print len(r[int(1+amax/h)-1:]),len(x)
 
 plt.plot(x,r[int(1+amax/h)-1:])
 plt.axis([0,tmax,min(r)-1,max(r)+1])
